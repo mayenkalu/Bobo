2077f8311eb412d35b27e1694b7f7700ff202b8a
chesahkalu <chesahkalu@gmail.com>
2023-05-21 00:56:31 +0100

    enviromental variables set for mysql database

--- Bobo/settings.py
+++ Bobo/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,9 +82,9 @@ WSGI_APPLICATION = 'Bobo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'env_vars',
-        'USER': 'env_vars',
-        'PASSWORD': 'env_vars',
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
         'HOST': 'localhost',
         'PORT': '3306',
     }
