f7957f1592f802268609ba24694ef50b6fb0382d
chesahkalu <chesahkalu@gmail.com>
2023-05-22 01:07:41 +0100

    tests created for views functions, Tests the views by checking the response status code, template used, and form instance. It also tests the form submission by posting valid data and verifying the user is created

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,70 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import User
+from django.urls import reverse
 
-# Create your tests here.
+class AuthTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser',
+            email='testuser@test.com',
+            password='testpassword'
+        )
+
+    def test_signup_view(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/signup.html')
+
+    def test_signup_view_post(self):
+        response = self.client.post(reverse('signup'), {
+            'username': 'testuser2',
+            'email': 'testuser2@test.com',
+            'password1': 'testpassword2',
+            'password2': 'testpassword2',
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('home'))
+
+    def test_login_view(self):
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/login.html')
+
+    def test_login_view_post(self):
+        response = self.client.post(reverse('login'), {
+            'username': 'testuser',
+            'password': 'testpassword',
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('home'))
+
+    def test_home_view(self):
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/home.html')
+
+    def test_logout_view(self):
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(reverse('logout'))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('login'))
+
+    def test_delete_account_view(self):
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(reverse('delete_account'))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('delete_account_confirm'))
+
+    def test_delete_account_confirm_view(self):
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(reverse('delete_account_confirm'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/delete_account_confirm.html')
+
+    def test_delete_account_confirm_view_post(self):
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.post(reverse('delete_account_confirm'))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('login'))
