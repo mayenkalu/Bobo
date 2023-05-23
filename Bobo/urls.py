61c1564f32c6d730964c240e586172a42c78c9b6
chesahkalu <chesahkalu@gmail.com>
2023-05-23 23:15:31 +0100

    react frontend project started. Api viewset for users created with serialization to JSON

--- Bobo/urls.py
+++ Bobo/urls.py
@@ -14,11 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# project/urls.py
+
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from accounts.api import UserViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
+    path('api/', include(router.urls)),
+    path('accounts/', include('accounts.urls')),  # Keep your existing URLs
+    # ...other urls
 ]
-
