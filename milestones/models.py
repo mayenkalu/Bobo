f47d357000263a4a6c39c80084cbf9bc3499f303
chesahkalu <chesahkalu@gmail.com>
2023-05-30 11:28:50 +0100

    modelss updated with progress model

--- milestones/models.py
+++ milestones/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 from django.db import models
 from babies.models import Baby  # import BabyProfile model
 
@@ -14,3 +14,10 @@ class Activity(models.Model):
 class NutritionGuide(models.Model):
     month = models.IntegerField()
     guide = models.TextField()
+
+class Progress(models.Model):
+    baby = models.OneToOneField(Baby, on_delete=models.CASCADE)
+    report = models.TextField()
+
+    def __str__(self):
+        return f'Progress for {self.baby.name} at {timezone.now()}'
