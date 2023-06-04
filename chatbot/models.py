10d3f62d61069bc062c1933735c1a6447c2af460
chesahkalu <chesahkalu@gmail.com>
2023-06-04 20:24:37 +0100

    chatbot app created, models added

---
+++ chatbot/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
