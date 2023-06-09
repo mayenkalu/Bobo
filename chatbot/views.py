54a57951100a001485002f291c3d84f7777ed106
chesahkalu <chesahkalu@gmail.com>
2023-06-09 10:11:18 +0100

    chatbot app bug fix

--- chatbot/views.py
+++ chatbot/views.py
@@ -4,6 +4,8 @@ from chatbot.models import Message
 import requests
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
+from django.shortcuts import render
+
 
 def chatbot(request):
     if request.method == 'POST':
@@ -15,23 +17,25 @@ def chatbot(request):
         api_key = os.getenv('OPENAI_API_KEY')
 
         if api_key:
-            # Generate chatbot response using the OpenAI Playground API
+            # Generate chatbot response using the OpenAI API
             response = requests.post(
-                'https://api.openai.com/v1/playground/chat/completions',
+                'https://api.openai.com/v1/engines/davinci-codex/completions',
                 headers={
                     'Authorization': f'Bearer {api_key}',
                     'Content-Type': 'application/json',
                 },
                 json={
-                    'messages': [{'role': 'user', 'content': user_message}],
+                    'prompt': user_message,
+                    'max_tokens': 60,
                 }
             )
-
             # Save the chatbot response to the database
-            Message.objects.create(user=None, text=response.json()['choices'][0]['message']['content'])
-
-            return JsonResponse({'response': response.json()['choices'][0]['message']['content']})
+            Message.objects.create(user=None, text=response.json()['choices'][0]['text'])
+            
+            return JsonResponse({'response': response.json()['choices'][0]['text']})
         else:
             return JsonResponse({'error': 'OpenAI API key not found'})
+    elif request.method == 'GET':
+        return render(request, 'chatbot/chatbot.html')
     else:
         return JsonResponse({'error': 'Invalid request method'})
